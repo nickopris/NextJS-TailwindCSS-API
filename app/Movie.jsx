6c8c70741b514ea56ee946985807a8573076ed10
NickO <nick@digitaltrotter.com>
2023-04-02 17:42:52 +0300

    Initial

---
+++ app/Movie.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import Image from "next/image"
+import { parseISO, format } from 'date-fns';
+
+export default function Movie({id, title, poster_path, release_date}) {
+    const imagePath ='https://image.tmdb.org/t/p/original/'
+    const date = parseISO(release_date);
+    return(
+        <div>
+            <Link href={`/${id}`} >
+                <Image src={imagePath + poster_path} width={400} height={400} />
+            </Link>
+            <h1 className="text-2xl">{title}</h1>
+            <p>Released: {format(date, 'd MMMM YYY')}</p>
+        </div>
+    )
+}
---
+++ app/[movie]/page.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+export default async function MovieDetail({ params }) {
+    const { movie } = params
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
+    const res = await data.json()
+    console.log(res)
+    const imagePath ='https://image.tmdb.org/t/p/original/'
+
+    return (
+        <section class="bg-white dark:bg-gray-900">
+        <div class=" max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:pt-28">
+            <h1 className="text-3xl">{res.title}</h1>
+            <Image src={imagePath + res.poster_path} width={800} height={800} />
+            <p>{ res.overview }</p>
+            <p>Rating: {res.vote_average} / {res.vote_count} votes</p>
+        </div>
+        </section>
+    )
+}
